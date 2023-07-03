09a38251cedf7ecaead3b3c078757c0133e53ebf
banqueter <08-housing-bigger@icloud.com>
2023-07-03 02:16:40 +0100

    Added btnStand & btnHit functionalities as well as findWinner, scorer and dealerPlay functions

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const app = (function () {
   const game = {};
   const suits = [`spades`, `hearts`, `clubs`, `diams`];
   const ranks = [`A`, 2, 3, 4, 5, 6, 7, 8, 9, 10, `J`, `Q`, `K`];
+  // const ranks = [`A`, `A`, `A`, `A`, `A`, 2, `J`, `Q`, `K`];
+
+  const score = [];
+
   const organizedCardDeck = [].concat(
     ...suits.map((suit) =>
       ranks.map((rank) => ({
@@ -26,7 +30,6 @@ const app = (function () {
 
   function buildDeck() {
     game.cardDeck = shuffle(organizedCardDeck);
-    console.log(game.cardDeck);
   }
 
   function init() {
@@ -39,11 +42,45 @@ const app = (function () {
   }
 
   function takeCard(hand, element, hidden) {
+    if (game.cardDeck.lenght === 0) {
+      buildDeck();
+      console.log(`New deck shuffled`);
+    }
     let temp = game.cardDeck.shift();
-    console.log(temp);
     hand.push(temp);
-    console.log(game);
     showCard(temp, element);
+    if (hidden) {
+      game.cardBack = document.createElement(`div`);
+    }
+    game.cardBack.classList.add(`cardB`);
+    element.append(game.cardBack);
+  }
+
+  function showCard(card, element) {
+    if (card != undefined) {
+      element.style.backgroundColour = `white`;
+      let div = document.createElement(`div`);
+      div.classList.add(`card`);
+      if (card.suit === `hearts` || card.suit === `diams`) {
+        div.classList.add(`red`);
+      } else div.classList.add(`black`);
+      let span1 = document.createElement(`div`);
+      span1.innerHTML = card.rank + `&` + card.suit + `;`;
+      span1.classList.add(`tiny`);
+      div.appendChild(span1);
+
+      let span2 = document.createElement(`div`);
+      span2.innerHTML = card.rank;
+      span2.classList.add(`big`);
+      div.appendChild(span2);
+
+      let span3 = document.createElement(`div`);
+      span3.innerHTML = `&` + card.suit + `;`;
+      span3.classList.add(`big`);
+      div.appendChild(span3);
+
+      element.appendChild(div);
+    }
   }
 
   function deal() {
@@ -51,11 +88,109 @@ const app = (function () {
     game.playerHand = [];
     game.start = true;
     turnOff(game.btnDeal);
+    game.dealerScore.textContent = `-`;
+    game.playerScore.textContent = `-`;
 
+    game.playerCards.innerHTML = ``;
+    game.dealerCards.innerHTML = ``;
+
+    takeCard(game.dealerHand, game.dealerCards, true);
     takeCard(game.dealerHand, game.dealerCards, false);
+    takeCard(game.playerHand, game.playerCards, false);
+    takeCard(game.playerHand, game.playerCards, false);
+    updateCount();
+  }
 
-    game.playerCards.innerHTML = `Deal`;
-    game.dealerCards.innerHTML = `Deal`;
+  function playerStand() {
+    dealerPlay();
+    turnOff(game.btnHit);
+    turnOff(game.btnStand);
+  }
+
+  function nextCard() {
+    takeCard(game.playerHand, game.playerCards, false);
+    updateCount();
+  }
+
+  function findWinner() {
+    let player = scorer(game.playerHand);
+    let dealer = scorer(game.dealerHand);
+    console.log(`PLayer: ${player}; Dealer: ${dealer}`);
+    if (player > 21) {
+      game.status.textContent = `You busted with ${player}`;
+    }
+    if (dealer > 21) {
+      game.status.textContent = `Dealer busted with ${dealer}`;
+    }
+    if (player === dealer) {
+      game.status.textContent = `Draw! No winners; player: ${player} and dealer: ${dealer}`;
+    }
+    if ((player <= 21 && player > dealer) || dealer > 21) {
+      game.status.textContent = `Congratulations! You are the winner; you sored ${player}`;
+    } else {
+      game.status.textContent = `The house wins with ${dealer}`;
+    }
+    turnOn(game.btnDeal);
+  }
+
+  function dealerPlay() {
+    let dealer = scorer(game.dealerHand);
+    game.cardBack.style.display = `none`;
+    game.status.textContent = `Dealer score ` + dealer + ` `;
+    if (dealer >= 17) {
+      game.dealerScore.textContent = dealer;
+      findWinner();
+    } else {
+      takeCard(game.dealerHand, game.dealerCards, false);
+      game.dealerScore.textContent = dealer;
+      dealerPlay();
+    }
+  }
+
+  function updateCount() {
+    let player = scorer(game.playerHand);
+    let dealer = scorer(game.dealerHand);
+    game.playerScore.textContent = player;
+    if (player < 21) {
+      turnOn(game.btnHit);
+      turnOn(game.btnStand);
+      game.status.textContent = `Stand or take another card`;
+    } else if (player > 21) {
+      findWinner();
+    } else {
+      game.status.textContent = `Dealer in play to 17 minimum.`;
+      dealerPlay(dealer);
+    }
+  }
+
+  function scoreAce(val, aces) {
+    if (val < 21) {
+      console.log(`val>21`);
+      return val;
+    } else if (aces > 0) {
+      console.log(`elif ace>0`);
+      aces--;
+      val -= 10;
+      return scoreAce(val, aces);
+    } else {
+      console.log(`else return value`);
+      return val;
+    }
+  }
+
+  function scorer(hand) {
+    let total = 0;
+    let ace = 0;
+    hand.forEach(function (card) {
+      if (card.rank === `A`) {
+        ace++;
+      }
+      total += Number(card.value);
+    });
+    if (ace > 0 && total > 21) {
+      total = scoreAce(total, ace);
+    }
+    return Number(total);
   }
 
   function addClicker() {
@@ -74,16 +209,8 @@ const app = (function () {
     btn.style.backgroundColour = `#000`;
   }
 
-  function playerStand() {
-    console.log(`Player Stand`);
-  }
-  function nextCard() {
-    console.log(`Next card`);
-  }
-
   function buildGameBoard() {
     game.main = document.querySelector(`#game`);
-    console.log(game);
 
     game.scoreBoard = document.createElement(`div`);
     game.scoreBoard.textContent = `Dealer 0 vs Player 0`;
@@ -137,6 +264,7 @@ const app = (function () {
     game.inputBet.style.width = `4em`;
     game.inputBet.style.fontSize = `1rem`;
     game.inputBet.style.marginTop = `0.3em`;
+    game.inputBet.style.marginRight = `0.5em`;
     game.inputBet.value = 0;
 
     game.btnBet = document.createElement(`button`);
